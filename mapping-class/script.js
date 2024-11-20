faf6ed75fb7725fd5ee542628868871ec74285d9
okonpatrick <okonpatrick2000@gmail.com>
2024-11-20 14:39:12 +0100

    javascript object class

---
+++ mapping-class/script.js
@@ -0,0 +1,41 @@
+ // Fetch JSON data and dynamically render the products
+ fetch('./data.json')
+ .then((response) => response.json())
+ .then((data) => renderProducts(data))
+ .catch((error) => console.error('Error fetching data:', error));
+
+function renderProducts(products) {
+ const container = document.getElementById('product-container');
+ container.innerHTML = products
+   .map(
+     (product) => `
+     <div class="product-card">
+       <div class="product-image">
+         <img src="${product.image}" alt="${product.name}">
+         ${product.isSale ? '<span class="badge">Sale</span>' : ''}
+         ${product.isNew ? '<span class="badge">New</span>' : ''}
+       </div>
+       <div class="product-info">
+         <img src=${product.name}></img>
+         <div class="product-code">Code: ${product.code}</div>
+         <div class="product-price">$${product.price.toFixed(2)}</div>
+         ${
+           product.oldPrice
+             ? `<div class="product-old-price">$${product.oldPrice.toFixed(
+                 2
+               )}</div>`
+             : ''
+         }
+       </div>
+     </div>
+   `
+   )
+   .join('');
+}
+
+
+// Constructor Function for Person objects
+
+const name = "W3Schools";
+let letter = name.at(6);
+console.log(letter)
--- project-work/script.js
+++ project-work/script.js
@@ -1,57 +1,121 @@
+//Assignment
+//Using prompt, program a simple calculator (user can perform addition, substraction, multiplication, division, modulus, log, cosine and sine function)
+//
 
 let x = 20;
-let y =10
+let y = 10;
 let z = x + y;
 console.log(z);
 
-var name = "Nsikak", number_ = "10", IsTrue = true;
+var name = "Nsikak",
+  number_ = "10",
+  IsTrue = true;
 var number_ = 10;
-let condition = false
-var varVariable = "This variable is declared with the var keyboard!"
+let condition = false;
+var varVariable = "This variable is declared with the var keyboard!";
 console.log(varVariable);
 
-let xscope = "global variable" 
-function GlobalFunction () {
-console.log(xscope);
+let xscope = "global variable";
+function GlobalFunction() {
+  console.log(xscope);
 }
 
-function NameOfFunction () {
-    let xscope = "global variable222";
-    if (true) {
-        let xscope = "scope variable new one";
-        console.log(xscope)
-    }
+function NameOfFunction() {
+  let xscope = "global variable222";
+  if (true) {
+    let xscope = "scope variable new one";
+    console.log(xscope);
+  }
 }
 
 console.log(xscope);
 NameOfFunction();
 
 const NameOfNewFunction = () => {
-   console.log("This is an alert message!")
-}
-NameOfNewFunction()
+  console.log("This is an alert message!");
+};
+NameOfNewFunction();
 
 let concateValue = 10 % 4;
 console.log(concateValue);
 
 let text1 = "What a very ";
 text1 += "nice day";
-console.log(typeof(text1))
+console.log(typeof text1);
 
 let letter1 = 5;
-let letter2 =2;
+let letter2 = 2;
 let finalResult = letter2 ^ letter1;
 console.log(finalResult);
 
-//this is an object of a car Object
-let carObject = {
-    steering: "automatic",
-    color: "red",
-    wheels: 4,
-    speed: 100,
-    engine: "V8",
-    doors: {
-        front: 4,
-    }
+//write a function that adds two numbers.
+
+//write a function calculates fahrenheit and converts to celsius.
+function fahrenheitCalculator(fahrenheitValue) {
+  return (5 / 9) * (fahrenheitValue - 32);
 }
-console.log(carObject.speed);
+let result = fahrenheitCalculator(10);
+console.log("The result is", result);
+
+//this is an object of a car Object with an object method
+let carObject = {
+  steering: "unautomatic",
+  color: "red",
+  wheels: 4,
+  speed: 100,
+  engine: "V8",
+  doors: {
+    front: 4,
+    sideDoor: {
+      Height: 20,
+    },
+  },
+  ObjectMethod: function functionName() {
+    return (
+      "The is" +
+      this.steering +
+      "and the color is " +
+      "with a " +
+      this.engine +
+      " Engine"
+    );
+  },
+};
+
+
+let Sentence = carObject.ObjectMethod();
+console.log(Sentence);
+console.log(carObject.steering);
+
+let objectVarible = {
+  name: "Nsikak",
+};
+
+// let operator = prompt("Please input the operator for calculation!");
+// let operator2 = prompt("Please input the operator2 for calculation!")
+
+// let mathresult = operator + operator2;
+// console.log(parseInt(mathresult));
+
+let newcarObject = {
+  steering: "unautomatic",
+  color: "red",
+  wheels: 4,
+  speed: 100,
+  engine: "V8",
+  doors: {
+    front: 4,
+  },
+};
+let newEngine = newcarObject;
+newEngine.engine = "V10";
+console.log(newEngine.engine);
+
+newcarObject.selfDriving = "Tesla";
+console.log(newcarObject.selfDriving);
+
+delete newcarObject.selfDriving;
+console.log(newcarObject);
+
+
+console.log(carObject.doors.sideDoor.Height)
